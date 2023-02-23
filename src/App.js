53a4c09b3e4577708a5c91a2d4d5bf6f39f858c6
Suriyaprakash-Github <yoursgump@gmail.com>
2023-02-23 18:01:21 +0530

    4th

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          First Deployment- gitpush
+          First Deployment- dynamic
         </a>
       </header>
     </div>
